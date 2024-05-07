a4d419d1c6c578e0eccde466182b973f0853e8e4
NouranAbdelaziz <nouran.abdelaziz@aucegpt.edu>
2024-05-07 19:14:02 +0300

    changed fifos and irqs offset and regenerated documentation and wrappers

--- hdl/rtl/bus_wrappers/EF_SPI_APB.pp.v
+++ hdl/rtl/bus_wrappers/EF_SPI_APB.pp.v
@@ -1,5 +1,5 @@
 /*
-	Copyright 2023 Mohamed Shalan
+	Copyright 2023 Efabless Corp.
 
 	Author: Mohamed Shalan (mshalan@efabless.com)
 
@@ -85,7 +85,8 @@
 
 module EF_SPI_APB #( 
 	parameter	
-		CDW = 8
+		CDW = 8,
+		FAW = 4
 ) (
 	input wire          PCLK,
                                         input wire          PRESETn,
@@ -98,22 +99,27 @@ module EF_SPI_APB #(
                                         output wire [31:0]  PRDATA,
                                         output wire         IRQ
 ,
-	input	[1-1:0]	dout,
-	output	[1-1:0]	din,
+	input	[1-1:0]	miso,
+	output	[1-1:0]	mosi,
 	output	[1-1:0]	csb,
 	output	[1-1:0]	sclk
 );
 
-	localparam	DATA_REG_OFFSET = 16'd0;
-	localparam	CFG_REG_OFFSET = 16'd8;
-	localparam	STATUS_REG_OFFSET = 16'd16;
-	localparam	CTRL_REG_OFFSET = 16'd24;
-	localparam	PR_REG_OFFSET = 16'd32;
-	localparam	IM_REG_OFFSET = 16'd3840;
-	localparam	MIS_REG_OFFSET = 16'd3844;
-	localparam	RIS_REG_OFFSET = 16'd3848;
-	localparam	IC_REG_OFFSET = 16'd3852;
-
+	localparam	RXDATA_REG_OFFSET = 16'h0000;
+	localparam	TXDATA_REG_OFFSET = 16'h0004;
+	localparam	CFG_REG_OFFSET = 16'h0008;
+	localparam	CTRL_REG_OFFSET = 16'h000C;
+	localparam	PR_REG_OFFSET = 16'h0010;
+	localparam	RX_FIFO_LEVEL_REG_OFFSET = 16'hFE00;
+	localparam	RX_FIFO_THRESHOLD_REG_OFFSET = 16'hFE04;
+	localparam	RX_FIFO_FLUSH_REG_OFFSET = 16'hFE08;
+	localparam	TX_FIFO_LEVEL_REG_OFFSET = 16'hFE10;
+	localparam	TX_FIFO_THRESHOLD_REG_OFFSET = 16'hFE14;
+	localparam	TX_FIFO_FLUSH_REG_OFFSET = 16'hFE18;
+	localparam	IM_REG_OFFSET = 16'hFF00;
+	localparam	MIS_REG_OFFSET = 16'hFF04;
+	localparam	RIS_REG_OFFSET = 16'hFF08;
+	localparam	IC_REG_OFFSET = 16'hFF0C;
 	wire		clk = PCLK;
 	wire		rst_n = PRESETn;
 
@@ -125,22 +131,29 @@ module EF_SPI_APB #(
 	wire [1-1:0]	CPOL;
 	wire [1-1:0]	CPHA;
 	wire [CDW-1:0]	clk_divider;
-	wire [1-1:0]	go;
+	wire [1-1:0]	wr;
+	wire [1-1:0]	rd;
 	wire [8-1:0]	datai;
 	wire [8-1:0]	datao;
-	wire [1-1:0]	busy;
-	wire [1-1:0]	done;
+	wire [1-1:0]	rx_en;
+	wire [1-1:0]	rx_flush;
+	wire [FAW-1:0]	rx_threshold;
+	wire [1-1:0]	rx_empty;
+	wire [1-1:0]	rx_full;
+	wire [1-1:0]	rx_level_above;
+	wire [FAW-1:0]	rx_level;
+	wire [1-1:0]	tx_flush;
+	wire [FAW-1:0]	tx_threshold;
+	wire [1-1:0]	tx_empty;
+	wire [1-1:0]	tx_full;
+	wire [1-1:0]	tx_level_below;
+	wire [FAW-1:0]	tx_level;
 	wire [1-1:0]	ss;
 
-
 	// Register Definitions
-	reg	[8-1:0]	DATA_REG;
-	wire	[8-1:0]	DATA_WIRE;
-	assign	DATA_WIRE = datao;
-	assign	datai = DATA_REG;
-	always @(posedge PCLK or negedge PRESETn) if(~PRESETn) DATA_REG <= 0;
-                                        else if(apb_we & (PADDR[16-1:0]==DATA_REG_OFFSET))
-                                            DATA_REG <= PWDATA[8-1:0];
+	wire	[8-1:0]	RXDATA_WIRE;
+
+	wire	[8-1:0]	TXDATA_WIRE;
 
 	reg [1:0]	CFG_REG;
 	assign	CPOL	=	CFG_REG[0 : 0];
@@ -149,77 +162,143 @@ module EF_SPI_APB #(
                                         else if(apb_we & (PADDR[16-1:0]==CFG_REG_OFFSET))
                                             CFG_REG <= PWDATA[2-1:0];
 
-	wire [2-1:0]	STATUS_WIRE;
-	assign	STATUS_WIRE[0 : 0] = done;
-	assign	STATUS_WIRE[1 : 1] = busy;
-
-	reg [1:0]	CTRL_REG;
-	assign	go	=	CTRL_REG[0 : 0];
+	reg [0:0]	CTRL_REG;
 	assign	ss	=	CTRL_REG[1 : 1];
 	always @(posedge PCLK or negedge PRESETn) if(~PRESETn) CTRL_REG <= 0;
-                                                else if(apb_we & (PADDR[16-1:0]==CTRL_REG_OFFSET))
-                                                    CTRL_REG <= PWDATA[2-1:0];
-                                                else
-                                                    CTRL_REG <= 2'h2 & CTRL_REG;
+                                        else if(apb_we & (PADDR[16-1:0]==CTRL_REG_OFFSET))
+                                            CTRL_REG <= PWDATA[1-1:0];
 
-	reg [15:0]	PR_REG;
+	reg [CDW-1:0]	PR_REG;
 	assign	clk_divider = PR_REG;
 	always @(posedge PCLK or negedge PRESETn) if(~PRESETn) PR_REG <= 'h2;
                                         else if(apb_we & (PADDR[16-1:0]==PR_REG_OFFSET))
-                                            PR_REG <= PWDATA[16-1:0];
+                                            PR_REG <= PWDATA[CDW-1:0];
 
-	reg [0:0] IM_REG;
-	reg [0:0] IC_REG;
-	reg [0:0] RIS_REG;
+	wire [FAW-1:0]	RX_FIFO_LEVEL_WIRE;
+	assign	RX_FIFO_LEVEL_WIRE[(FAW - 1) : 0] = rx_level;
 
-	wire[1-1:0]      MIS_REG	= RIS_REG & IM_REG;
+	reg [0:0]	RX_FIFO_THRESHOLD_REG;
+	assign	rx_threshold	=	RX_FIFO_THRESHOLD_REG[0 : 0];
+	always @(posedge PCLK or negedge PRESETn) if(~PRESETn) RX_FIFO_THRESHOLD_REG <= 0;
+                                        else if(apb_we & (PADDR[16-1:0]==RX_FIFO_THRESHOLD_REG_OFFSET))
+                                            RX_FIFO_THRESHOLD_REG <= PWDATA[1-1:0];
+
+	reg [0:0]	RX_FIFO_FLUSH_REG;
+	assign	rx_flush	=	RX_FIFO_FLUSH_REG[0 : 0];
+	always @(posedge PCLK or negedge PRESETn) if(~PRESETn) RX_FIFO_FLUSH_REG <= 0;
+                                                else if(apb_we & (PADDR[16-1:0]==RX_FIFO_FLUSH_REG_OFFSET))
+                                                    RX_FIFO_FLUSH_REG <= PWDATA[1-1:0];
+                                                else
+                                                    RX_FIFO_FLUSH_REG <= 1'h0 & RX_FIFO_FLUSH_REG;
+
+	wire [FAW-1:0]	TX_FIFO_LEVEL_WIRE;
+	assign	TX_FIFO_LEVEL_WIRE[(FAW - 1) : 0] = tx_level;
+
+	reg [0:0]	TX_FIFO_THRESHOLD_REG;
+	assign	tx_threshold	=	TX_FIFO_THRESHOLD_REG[0 : 0];
+	always @(posedge PCLK or negedge PRESETn) if(~PRESETn) TX_FIFO_THRESHOLD_REG <= 0;
+                                        else if(apb_we & (PADDR[16-1:0]==TX_FIFO_THRESHOLD_REG_OFFSET))
+                                            TX_FIFO_THRESHOLD_REG <= PWDATA[1-1:0];
+
+	reg [0:0]	TX_FIFO_FLUSH_REG;
+	assign	tx_flush	=	TX_FIFO_FLUSH_REG[0 : 0];
+	always @(posedge PCLK or negedge PRESETn) if(~PRESETn) TX_FIFO_FLUSH_REG <= 0;
+                                                else if(apb_we & (PADDR[16-1:0]==TX_FIFO_FLUSH_REG_OFFSET))
+                                                    TX_FIFO_FLUSH_REG <= PWDATA[1-1:0];
+                                                else
+                                                    TX_FIFO_FLUSH_REG <= 1'h0 & TX_FIFO_FLUSH_REG;
+
+	reg [5:0] IM_REG;
+	reg [5:0] IC_REG;
+	reg [5:0] RIS_REG;
+
+	wire[6-1:0]      MIS_REG	= RIS_REG & IM_REG;
 	always @(posedge PCLK or negedge PRESETn) if(~PRESETn) IM_REG <= 0;
                                         else if(apb_we & (PADDR[16-1:0]==IM_REG_OFFSET))
-                                            IM_REG <= PWDATA[1-1:0];
-	always @(posedge PCLK or negedge PRESETn) if(~PRESETn) IC_REG <= 1'b0;
+                                            IM_REG <= PWDATA[6-1:0];
+	always @(posedge PCLK or negedge PRESETn) if(~PRESETn) IC_REG <= 6'b0;
                                         else if(apb_we & (PADDR[16-1:0]==IC_REG_OFFSET))
-                                            IC_REG <= PWDATA[1-1:0];
+                                            IC_REG <= PWDATA[6-1:0];
                                         else
-                                            IC_REG <= 1'd0;
+                                            IC_REG <= 6'd0;
 
-	wire [0:0] Done = done;
+	wire [0:0] TXE = tx_empty;
+	wire [0:0] TXF = tx_full;
+	wire [0:0] RXF = rx_empty;
+	wire [0:0] RXF = rx_full;
+	wire [0:0] TXB = tx_level_below;
+	wire [0:0] RXA = rx_level_above;
 
 
 	integer _i_;
 	always @(posedge PCLK or negedge PRESETn) if(~PRESETn) RIS_REG <= 0; else begin
 		for(_i_ = 0; _i_ < 1; _i_ = _i_ + 1) begin
-			if(IC_REG[_i_]) RIS_REG[_i_] <= 1'b0; else if(Done[_i_ - 0] == 1'b1) RIS_REG[_i_] <= 1'b1;
+			if(IC_REG[_i_]) RIS_REG[_i_] <= 1'b0; else if(TXE[_i_ - 0] == 1'b1) RIS_REG[_i_] <= 1'b1;
+		end
+		for(_i_ = 1; _i_ < 2; _i_ = _i_ + 1) begin
+			if(IC_REG[_i_]) RIS_REG[_i_] <= 1'b0; else if(TXF[_i_ - 1] == 1'b1) RIS_REG[_i_] <= 1'b1;
+		end
+		for(_i_ = 2; _i_ < 3; _i_ = _i_ + 1) begin
+			if(IC_REG[_i_]) RIS_REG[_i_] <= 1'b0; else if(RXF[_i_ - 2] == 1'b1) RIS_REG[_i_] <= 1'b1;
+		end
+		for(_i_ = 3; _i_ < 4; _i_ = _i_ + 1) begin
+			if(IC_REG[_i_]) RIS_REG[_i_] <= 1'b0; else if(RXF[_i_ - 3] == 1'b1) RIS_REG[_i_] <= 1'b1;
+		end
+		for(_i_ = 4; _i_ < 5; _i_ = _i_ + 1) begin
+			if(IC_REG[_i_]) RIS_REG[_i_] <= 1'b0; else if(TXB[_i_ - 4] == 1'b1) RIS_REG[_i_] <= 1'b1;
+		end
+		for(_i_ = 5; _i_ < 6; _i_ = _i_ + 1) begin
+			if(IC_REG[_i_]) RIS_REG[_i_] <= 1'b0; else if(RXA[_i_ - 5] == 1'b1) RIS_REG[_i_] <= 1'b1;
 		end
 	end
 
 	assign IRQ = |MIS_REG;
 
 	EF_SPI #(
-		.CDW(CDW)
+		.CDW(CDW),
+		.FAW(FAW)
 	) instance_to_wrap (
 		.clk(clk),
 		.rst_n(rst_n),
 		.CPOL(CPOL),
 		.CPHA(CPHA),
 		.clk_divider(clk_divider),
-		.go(go),
+		.wr(wr),
+		.rd(rd),
 		.datai(datai),
 		.datao(datao),
-		.busy(busy),
-		.done(done),
+		.rx_en(rx_en),
+		.rx_flush(rx_flush),
+		.rx_threshold(rx_threshold),
+		.rx_empty(rx_empty),
+		.rx_full(rx_full),
+		.rx_level_above(rx_level_above),
+		.rx_level(rx_level),
+		.tx_flush(tx_flush),
+		.tx_threshold(tx_threshold),
+		.tx_empty(tx_empty),
+		.tx_full(tx_full),
+		.tx_level_below(tx_level_below),
+		.tx_level(tx_level),
 		.ss(ss),
-		.dout(dout),
-		.din(din),
+		.miso(miso),
+		.mosi(mosi),
 		.csb(csb),
 		.sclk(sclk)
 	);
 
 	assign	PRDATA = 
-			(PADDR[16-1:0] == DATA_REG_OFFSET)	? DATA_WIRE :
+			(PADDR[16-1:0] == RXDATA_REG_OFFSET)	? RXDATA_WIRE :
+			(PADDR[16-1:0] == TXDATA_REG_OFFSET)	? TXDATA_WIRE :
 			(PADDR[16-1:0] == CFG_REG_OFFSET)	? CFG_REG :
-			(PADDR[16-1:0] == STATUS_REG_OFFSET)	? STATUS_WIRE :
 			(PADDR[16-1:0] == CTRL_REG_OFFSET)	? CTRL_REG :
 			(PADDR[16-1:0] == PR_REG_OFFSET)	? PR_REG :
+			(PADDR[16-1:0] == RX_FIFO_LEVEL_REG_OFFSET)	? RX_FIFO_LEVEL_WIRE :
+			(PADDR[16-1:0] == RX_FIFO_THRESHOLD_REG_OFFSET)	? RX_FIFO_THRESHOLD_REG :
+			(PADDR[16-1:0] == RX_FIFO_FLUSH_REG_OFFSET)	? RX_FIFO_FLUSH_REG :
+			(PADDR[16-1:0] == TX_FIFO_LEVEL_REG_OFFSET)	? TX_FIFO_LEVEL_WIRE :
+			(PADDR[16-1:0] == TX_FIFO_THRESHOLD_REG_OFFSET)	? TX_FIFO_THRESHOLD_REG :
+			(PADDR[16-1:0] == TX_FIFO_FLUSH_REG_OFFSET)	? TX_FIFO_FLUSH_REG :
 			(PADDR[16-1:0] == IM_REG_OFFSET)	? IM_REG :
 			(PADDR[16-1:0] == MIS_REG_OFFSET)	? MIS_REG :
 			(PADDR[16-1:0] == RIS_REG_OFFSET)	? RIS_REG :
@@ -228,4 +307,8 @@ module EF_SPI_APB #(
 
 	assign	PREADY = 1'b1;
 
+	assign	RXDATA_WIRE = datao;
+	assign	rd = (apb_re & (PADDR[16-1:0] == RXDATA_REG_OFFSET));
+	assign	wdata = PWDATA;
+	assign	wr = (apb_we & (PADDR[16-1:0] == TXDATA_REG_OFFSET));
 endmodule
--- hdl/rtl/bus_wrappers/EF_SPI_APB.v
+++ hdl/rtl/bus_wrappers/EF_SPI_APB.v
@@ -1,5 +1,5 @@
 /*
-	Copyright 2023 Mohamed Shalan
+	Copyright 2023 Efabless Corp.
 
 	Author: Mohamed Shalan (mshalan@efabless.com)
 
@@ -28,25 +28,31 @@
 
 module EF_SPI_APB #( 
 	parameter	
-		CDW = 8
+		CDW = 8,
+		FAW = 4
 ) (
 	`APB_SLAVE_PORTS,
-	input	[1-1:0]	dout,
-	output	[1-1:0]	din,
+	input	[1-1:0]	miso,
+	output	[1-1:0]	mosi,
 	output	[1-1:0]	csb,
 	output	[1-1:0]	sclk
 );
 
-	localparam	DATA_REG_OFFSET = `APB_AW'd0;
-	localparam	CFG_REG_OFFSET = `APB_AW'd8;
-	localparam	STATUS_REG_OFFSET = `APB_AW'd16;
-	localparam	CTRL_REG_OFFSET = `APB_AW'd24;
-	localparam	PR_REG_OFFSET = `APB_AW'd32;
-	localparam	IM_REG_OFFSET = `APB_AW'd3840;
-	localparam	MIS_REG_OFFSET = `APB_AW'd3844;
-	localparam	RIS_REG_OFFSET = `APB_AW'd3848;
-	localparam	IC_REG_OFFSET = `APB_AW'd3852;
-
+	localparam	RXDATA_REG_OFFSET = `APB_AW'h0000;
+	localparam	TXDATA_REG_OFFSET = `APB_AW'h0004;
+	localparam	CFG_REG_OFFSET = `APB_AW'h0008;
+	localparam	CTRL_REG_OFFSET = `APB_AW'h000C;
+	localparam	PR_REG_OFFSET = `APB_AW'h0010;
+	localparam	RX_FIFO_LEVEL_REG_OFFSET = `APB_AW'hFE00;
+	localparam	RX_FIFO_THRESHOLD_REG_OFFSET = `APB_AW'hFE04;
+	localparam	RX_FIFO_FLUSH_REG_OFFSET = `APB_AW'hFE08;
+	localparam	TX_FIFO_LEVEL_REG_OFFSET = `APB_AW'hFE10;
+	localparam	TX_FIFO_THRESHOLD_REG_OFFSET = `APB_AW'hFE14;
+	localparam	TX_FIFO_FLUSH_REG_OFFSET = `APB_AW'hFE18;
+	localparam	IM_REG_OFFSET = `APB_AW'hFF00;
+	localparam	MIS_REG_OFFSET = `APB_AW'hFF04;
+	localparam	RIS_REG_OFFSET = `APB_AW'hFF08;
+	localparam	IC_REG_OFFSET = `APB_AW'hFF0C;
 	wire		clk = PCLK;
 	wire		rst_n = PRESETn;
 
@@ -56,85 +62,150 @@ module EF_SPI_APB #(
 	wire [1-1:0]	CPOL;
 	wire [1-1:0]	CPHA;
 	wire [CDW-1:0]	clk_divider;
-	wire [1-1:0]	go;
+	wire [1-1:0]	wr;
+	wire [1-1:0]	rd;
 	wire [8-1:0]	datai;
 	wire [8-1:0]	datao;
-	wire [1-1:0]	busy;
-	wire [1-1:0]	done;
+	wire [1-1:0]	rx_en;
+	wire [1-1:0]	rx_flush;
+	wire [FAW-1:0]	rx_threshold;
+	wire [1-1:0]	rx_empty;
+	wire [1-1:0]	rx_full;
+	wire [1-1:0]	rx_level_above;
+	wire [FAW-1:0]	rx_level;
+	wire [1-1:0]	tx_flush;
+	wire [FAW-1:0]	tx_threshold;
+	wire [1-1:0]	tx_empty;
+	wire [1-1:0]	tx_full;
+	wire [1-1:0]	tx_level_below;
+	wire [FAW-1:0]	tx_level;
 	wire [1-1:0]	ss;
 
-
 	// Register Definitions
-	reg	[8-1:0]	DATA_REG;
-	wire	[8-1:0]	DATA_WIRE;
-	assign	DATA_WIRE = datao;
-	assign	datai = DATA_REG;
-	`APB_REG(DATA_REG, 0, 8)
+	wire	[8-1:0]	RXDATA_WIRE;
+
+	wire	[8-1:0]	TXDATA_WIRE;
 
 	reg [1:0]	CFG_REG;
 	assign	CPOL	=	CFG_REG[0 : 0];
 	assign	CPHA	=	CFG_REG[1 : 1];
 	`APB_REG(CFG_REG, 0, 2)
 
-	wire [2-1:0]	STATUS_WIRE;
-	assign	STATUS_WIRE[0 : 0] = done;
-	assign	STATUS_WIRE[1 : 1] = busy;
-
-	reg [1:0]	CTRL_REG;
-	assign	go	=	CTRL_REG[0 : 0];
+	reg [0:0]	CTRL_REG;
 	assign	ss	=	CTRL_REG[1 : 1];
-	`APB_REG_AC(CTRL_REG, 0, 2, 2'h2)
+	`APB_REG(CTRL_REG, 0, 1)
 
-	reg [15:0]	PR_REG;
+	reg [CDW-1:0]	PR_REG;
 	assign	clk_divider = PR_REG;
-	`APB_REG(PR_REG, 'h2, 16)
+	`APB_REG(PR_REG, 'h2, CDW)
 
-	reg [0:0] IM_REG;
-	reg [0:0] IC_REG;
-	reg [0:0] RIS_REG;
+	wire [FAW-1:0]	RX_FIFO_LEVEL_WIRE;
+	assign	RX_FIFO_LEVEL_WIRE[(FAW - 1) : 0] = rx_level;
 
-	`APB_MIS_REG(1)
-	`APB_REG(IM_REG, 0, 1)
-	`APB_IC_REG(1)
+	reg [0:0]	RX_FIFO_THRESHOLD_REG;
+	assign	rx_threshold	=	RX_FIFO_THRESHOLD_REG[0 : 0];
+	`APB_REG(RX_FIFO_THRESHOLD_REG, 0, 1)
 
-	wire [0:0] Done = done;
+	reg [0:0]	RX_FIFO_FLUSH_REG;
+	assign	rx_flush	=	RX_FIFO_FLUSH_REG[0 : 0];
+	`APB_REG_AC(RX_FIFO_FLUSH_REG, 0, 1, 1'h0)
+
+	wire [FAW-1:0]	TX_FIFO_LEVEL_WIRE;
+	assign	TX_FIFO_LEVEL_WIRE[(FAW - 1) : 0] = tx_level;
+
+	reg [0:0]	TX_FIFO_THRESHOLD_REG;
+	assign	tx_threshold	=	TX_FIFO_THRESHOLD_REG[0 : 0];
+	`APB_REG(TX_FIFO_THRESHOLD_REG, 0, 1)
+
+	reg [0:0]	TX_FIFO_FLUSH_REG;
+	assign	tx_flush	=	TX_FIFO_FLUSH_REG[0 : 0];
+	`APB_REG_AC(TX_FIFO_FLUSH_REG, 0, 1, 1'h0)
+
+	reg [5:0] IM_REG;
+	reg [5:0] IC_REG;
+	reg [5:0] RIS_REG;
+
+	`APB_MIS_REG(6)
+	`APB_REG(IM_REG, 0, 6)
+	`APB_IC_REG(6)
+
+	wire [0:0] TXE = tx_empty;
+	wire [0:0] TXF = tx_full;
+	wire [0:0] RXF = rx_empty;
+	wire [0:0] RXF = rx_full;
+	wire [0:0] TXB = tx_level_below;
+	wire [0:0] RXA = rx_level_above;
 
 
 	integer _i_;
 	`APB_BLOCK(RIS_REG, 0) else begin
 		for(_i_ = 0; _i_ < 1; _i_ = _i_ + 1) begin
-			if(IC_REG[_i_]) RIS_REG[_i_] <= 1'b0; else if(Done[_i_ - 0] == 1'b1) RIS_REG[_i_] <= 1'b1;
+			if(IC_REG[_i_]) RIS_REG[_i_] <= 1'b0; else if(TXE[_i_ - 0] == 1'b1) RIS_REG[_i_] <= 1'b1;
+		end
+		for(_i_ = 1; _i_ < 2; _i_ = _i_ + 1) begin
+			if(IC_REG[_i_]) RIS_REG[_i_] <= 1'b0; else if(TXF[_i_ - 1] == 1'b1) RIS_REG[_i_] <= 1'b1;
+		end
+		for(_i_ = 2; _i_ < 3; _i_ = _i_ + 1) begin
+			if(IC_REG[_i_]) RIS_REG[_i_] <= 1'b0; else if(RXF[_i_ - 2] == 1'b1) RIS_REG[_i_] <= 1'b1;
+		end
+		for(_i_ = 3; _i_ < 4; _i_ = _i_ + 1) begin
+			if(IC_REG[_i_]) RIS_REG[_i_] <= 1'b0; else if(RXF[_i_ - 3] == 1'b1) RIS_REG[_i_] <= 1'b1;
+		end
+		for(_i_ = 4; _i_ < 5; _i_ = _i_ + 1) begin
+			if(IC_REG[_i_]) RIS_REG[_i_] <= 1'b0; else if(TXB[_i_ - 4] == 1'b1) RIS_REG[_i_] <= 1'b1;
+		end
+		for(_i_ = 5; _i_ < 6; _i_ = _i_ + 1) begin
+			if(IC_REG[_i_]) RIS_REG[_i_] <= 1'b0; else if(RXA[_i_ - 5] == 1'b1) RIS_REG[_i_] <= 1'b1;
 		end
 	end
 
 	assign IRQ = |MIS_REG;
 
 	EF_SPI #(
-		.CDW(CDW)
+		.CDW(CDW),
+		.FAW(FAW)
 	) instance_to_wrap (
 		.clk(clk),
 		.rst_n(rst_n),
 		.CPOL(CPOL),
 		.CPHA(CPHA),
 		.clk_divider(clk_divider),
-		.go(go),
+		.wr(wr),
+		.rd(rd),
 		.datai(datai),
 		.datao(datao),
-		.busy(busy),
-		.done(done),
+		.rx_en(rx_en),
+		.rx_flush(rx_flush),
+		.rx_threshold(rx_threshold),
+		.rx_empty(rx_empty),
+		.rx_full(rx_full),
+		.rx_level_above(rx_level_above),
+		.rx_level(rx_level),
+		.tx_flush(tx_flush),
+		.tx_threshold(tx_threshold),
+		.tx_empty(tx_empty),
+		.tx_full(tx_full),
+		.tx_level_below(tx_level_below),
+		.tx_level(tx_level),
 		.ss(ss),
-		.dout(dout),
-		.din(din),
+		.miso(miso),
+		.mosi(mosi),
 		.csb(csb),
 		.sclk(sclk)
 	);
 
 	assign	PRDATA = 
-			(PADDR[`APB_AW-1:0] == DATA_REG_OFFSET)	? DATA_WIRE :
+			(PADDR[`APB_AW-1:0] == RXDATA_REG_OFFSET)	? RXDATA_WIRE :
+			(PADDR[`APB_AW-1:0] == TXDATA_REG_OFFSET)	? TXDATA_WIRE :
 			(PADDR[`APB_AW-1:0] == CFG_REG_OFFSET)	? CFG_REG :
-			(PADDR[`APB_AW-1:0] == STATUS_REG_OFFSET)	? STATUS_WIRE :
 			(PADDR[`APB_AW-1:0] == CTRL_REG_OFFSET)	? CTRL_REG :
 			(PADDR[`APB_AW-1:0] == PR_REG_OFFSET)	? PR_REG :
+			(PADDR[`APB_AW-1:0] == RX_FIFO_LEVEL_REG_OFFSET)	? RX_FIFO_LEVEL_WIRE :
+			(PADDR[`APB_AW-1:0] == RX_FIFO_THRESHOLD_REG_OFFSET)	? RX_FIFO_THRESHOLD_REG :
+			(PADDR[`APB_AW-1:0] == RX_FIFO_FLUSH_REG_OFFSET)	? RX_FIFO_FLUSH_REG :
+			(PADDR[`APB_AW-1:0] == TX_FIFO_LEVEL_REG_OFFSET)	? TX_FIFO_LEVEL_WIRE :
+			(PADDR[`APB_AW-1:0] == TX_FIFO_THRESHOLD_REG_OFFSET)	? TX_FIFO_THRESHOLD_REG :
+			(PADDR[`APB_AW-1:0] == TX_FIFO_FLUSH_REG_OFFSET)	? TX_FIFO_FLUSH_REG :
 			(PADDR[`APB_AW-1:0] == IM_REG_OFFSET)	? IM_REG :
 			(PADDR[`APB_AW-1:0] == MIS_REG_OFFSET)	? MIS_REG :
 			(PADDR[`APB_AW-1:0] == RIS_REG_OFFSET)	? RIS_REG :
@@ -143,4 +214,8 @@ module EF_SPI_APB #(
 
 	assign	PREADY = 1'b1;
 
+	assign	RXDATA_WIRE = datao;
+	assign	rd = (apb_re & (PADDR[`APB_AW-1:0] == RXDATA_REG_OFFSET));
+	assign	wdata = PWDATA;
+	assign	wr = (apb_we & (PADDR[`APB_AW-1:0] == TXDATA_REG_OFFSET));
 endmodule
